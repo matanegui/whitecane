5d293b28f4469ef820c4ec041f88cdaa76145fe6
matias <matias.cincunegui@gmail.com>
2017-10-13 15:57:42 -0300

    Add typings. Fix collision

--- src/index.ts
+++ src/index.ts
@@ -22,27 +22,26 @@ const ANIMATIONS = {
     RIGHT: [40]
   }
 }
-const COLLISION_DELAY = 30;
+const COLLISION_DELAY = 50;
 
 // Utils
-function isPointInRect(x, y, rx, ry, rw, rh) {
+function isPointInRect(x: number, y: number, rx: number, ry: number, rw: number, rh: number): boolean {
   return x >= rx && x < rx + rw && y >= ry && y < ry + rh
 }
 
-function areCollidingFull(ax, ay, bx, by) {
+function areCollidingFull(ax: number, ay: number, bx: number, by: number): boolean {
   return Math.floor(ax) === Math.floor(bx) && Math.floor(ay) === Math.floor(by)
 }
 
 //Globals
-let t = 0
-let delta = 0
-const mouseData = {
+let t: number = 0
+let delta: number = 0
+let mouseData: MouseData = {
   x: 0,
   y: 0,
   p: 0,
   lastClickStamp: 0
 }
-
 const scene = {
   tilemap: {
     x: 0,
@@ -67,8 +66,8 @@ const scene = {
     x: 112,
     y: 40,
     directions: [
-      {animationId: "RIGHT", bounce: "DOWN"},
-      {animationId: "DOWN", bounce: "RIGHT"}
+      { animationId: "RIGHT", bounce: "DOWN" },
+      { animationId: "DOWN", bounce: "RIGHT" }
     ]
   }],
   bends: [],
@@ -79,9 +78,9 @@ const scene = {
   }
 }
 
-function initScene(scene) {
+function initScene(scene): void {
   //Get scene data from map
-  const mapPadding = {
+  const mapPadding: Padding = {
     top: 3,
     bottom: 1,
     left: 2,
@@ -90,10 +89,10 @@ function initScene(scene) {
   const mapW = 30
   const mapH = 17
   const spriteEntityMap = {
-    6: { type: "BEND", directions: { up: true, right: false } },
-    8: { type: "BEND", directions: { up: true, right: true } },
-    10: { type: "BEND", directions: { up: false, right: false } },
-    12: { type: "BEND", directions: { up: false, right: true } },
+    6: { type: "BEND", directions: { xvPositive: true, yvPositive: false } },
+    8: { type: "BEND", directions: { xvPositive: true, yvPositive: true } },
+    10: { type: "BEND", directions: { xvPositive: false, yvPositive: false } },
+    12: { type: "BEND", directions: { xvPositive: false, yvPositive: true } },
     64: { type: "HOME", color: "GREEN" },
     66: { type: "HOME", color: "BLUE" },
     68: { type: "HOME", color: "ORANGE" },
@@ -105,8 +104,7 @@ function initScene(scene) {
       if (entityData) {
         switch (entityData.type) {
           case "BEND":
-            scene.bends.push({ x: x * TILE_SIZE, y: entityData.directions.up ? y * TILE_SIZE + 16 : y * TILE_SIZE - 16, directions: entityData.directions, type: entityData.type })
-            scene.bends.push({ x: entityData.directions.right ? x * TILE_SIZE - 16 : x * TILE_SIZE + 16, y: y * TILE_SIZE, directions: entityData.directions, type: entityData.type })
+            scene.bends.push({ x: x * TILE_SIZE, y: y * TILE_SIZE, directions: entityData.directions, type: entityData.type })
         }
       }
     }
@@ -123,93 +121,95 @@ function initScene(scene) {
   })
 }
 
-function getInput(mouseData) {
+function getInput(mouseData: MouseData): MouseData {
   const mData = mouse()
   const timestamp = time()
-  mouseData.x = mData[0]
-  mouseData.y = mData[1]
-  mouseData.p = mData[2] && (mouseData.lastClickStamp === 0 || timestamp - mouseData.lastClickStamp > 50) ? 1 : 0
-  mouseData.lastClickStamp = mData[2] ? timestamp : mouseData.lastClickStamp
-}
-
-function update(input, scene, delta) {
-
-  function updateCursorPosition(mouseData, cursor) {
-    cursor.x = Math.floor((mouseData.x) / 16) * 16
-    cursor.y = Math.floor((mouseData.y + 8) / 16) * 16 - 8
+  return {
+    x: mData[0],
+    y: mData[1],
+    p: mData[2] && (mouseData.lastClickStamp === 0 || timestamp - mouseData.lastClickStamp > 50) ? 1 : 0,
+    lastClickStamp: mData[2] ? timestamp : mouseData.lastClickStamp
   }
 
-  function onBarrierClick(mouseData, barriers) {
+}
+
+function update(input, scene, delta): void {
+
+  function getCursorPosition(mouseData: MouseData): Point {
+    return {
+      x: Math.floor((mouseData.x) / 16) * 16,
+      y: Math.floor((mouseData.y + 8) / 16) * 16 - 8
+    }
+  }
+
+  function onBarrierClick(mouseData: MouseData, barriers): void {
     barriers.forEach(function (barrier) {
       if (mouseData.p && isPointInRect(mouseData.x, mouseData.y, barrier.x, barrier.y, TILE_SCALE * TILE_SIZE, TILE_SCALE * TILE_SIZE)) {
         barrier.directionIndex = barrier.directionIndex + 1 < barrier.directions.length
           ? barrier.directionIndex + 1
           : 0
-        const direction = barrier.directions[barrier.directionIndex]
+        const direction: { animationId: string, bounce: string } = barrier.directions[barrier.directionIndex]
         barrier.animationId = direction.animationId
-        barrier.bounce = direction.bounce      
+        barrier.bounce = direction.bounce
       }
     })
   }
 
   function move(walkers, barriers, bends, delta) {
 
-    function isCollisionDelayOver(timestamp){
+    function isCollisionDelayOver(timestamp) {
       return time() - timestamp > COLLISION_DELAY;
+    }
+
+    function bounceWalker(walker, bounceDirection: string): void {
+      walker.velocityX = 0
+      walker.velocityY = 0
+      walker.lastCollisionTimestamp = time()
+      switch (bounceDirection) {
+        case "UP":
+          walker.velocityY = -walker.speed
+          break;
+        case "RIGHT":
+          walker.velocityX = walker.speed
+          break;
+        case "DOWN":
+          walker.velocityY = walker.speed
+          break;
+        case "LEFT":
+          walker.velocityX = -walker.speed
+          break;
+      }
+      walker.animationId = bounceDirection
     }
 
     walkers.forEach(function (walker) {
       //Future positions (applying movement)
-      const mx = walker.x + walker.velocityX * delta
-      const my = walker.y + walker.velocityY * delta
-      //Check for bend collision and redirect walker
       bends.forEach(function (bend) {
-        if (areCollidingFull(mx + 16 * (walker.velocityX ? walker.velocityX < 0 ? -1 : 1 : 0), my + 16 * (walker.velocityY ? walker.velocityY < 0 ? -1 : 1 : 0), bend.x, bend.y)) {
+        if (areCollidingFull(bend.x, bend.y, walker.x, walker.y) && isCollisionDelayOver(walker.lastCollisionTimestamp)) {
+          walker.lastCollisionTimestamp = time()
+          let bounce: string
           if (walker.velocityX !== 0) {
-            walker.velocityY = walker.speed * (bend.directions.up ? -1 : 1)
-            walker.velocityX = 0
-            walker.animationId = bend.directions.up ? "UP" : "DOWN"
-            walker.y = my
+            bounce = bend.directions.yvPositive ? "UP" : "DOWN"
           }
           else if (walker.velocityY !== 0) {
-            walker.velocityX = walker.speed * (bend.directions.right ? 1 : -1)
-            walker.velocityY = 0
-            walker.animationId = bend.directions.right ? "RIGHT" : "LEFT"
-            walker.x = mx
+            bounce = bend.directions.XvPositive ? "RIGHT" : "LEFT"
           }
-        }
-        else {
-          walker.x = mx
-          walker.y = my
+          bounceWalker(walker,bounce)
         }
       })
+
       //Check for barrier collision and redirect walker
       barriers.forEach(function (barrier) {
         if (areCollidingFull(barrier.x, barrier.y, walker.x, walker.y) && isCollisionDelayOver(walker.lastCollisionTimestamp)) {
-          walker.velocityX = 0
-          walker.velocityY = 0
-          walker.lastCollisionTimestamp = time()
-          switch (barrier.bounce) {
-            case "UP":
-              walker.velocityY = -walker.speed
-            break;
-            case "RIGHT":
-              walker.velocityX = walker.speed
-            break;
-            case "DOWN":
-              walker.velocityY = walker.speed
-            break;
-            case "LEFT":
-              walker.velocityX = -walker.speed
-            break;
-          }
-          walker.animationId = barrier.bounce
+          bounceWalker(walker,barrier.bounce)
         }
       })
+      walker.x = walker.x + walker.velocityX * delta
+      walker.y = walker.y + walker.velocityY * delta
     })
   }
 
-  function animate(scene, delta) {
+  function animate(scene, delta: number): void {
     function animateEntity(entity, delta) {
       const a = ANIMATIONS[entity.type][entity.animationId]
       const aDelta = delta * 100
@@ -229,22 +229,25 @@ function update(input, scene, delta) {
     })
   }
 
-  updateCursorPosition(input, scene.cursor)
+  scene.cursor = getCursorPosition(input)
   onBarrierClick(input, scene.barriers)
   move(scene.walkers, scene.barriers, scene.bends, delta)
   animate(scene, delta)
 }
 
-function draw(scene) {
+function draw(scene): void {
+  //Draw map
   map(scene.tilemap.x, scene.tilemap.y)
+  //Draw walkers
   scene.walkers.forEach(function (walker) {
     spr(walker.sprite, walker.x, walker.y - 8, 0, 1, 0, 0, TILE_SCALE, TILE_SCALE)
   })
+  //Draw barriers
   scene.barriers.forEach(function (barrier) {
     spr(barrier.sprite, barrier.x, barrier.y, 0, 1, 0, 0, TILE_SCALE, TILE_SCALE)
   })
   //Draw cursor
-  let cursorSpr = 44
+  let cursorSpr: number = 44
   scene.barriers.forEach(function (barrier) {
     if (isPointInRect(scene.cursor.x, scene.cursor.y, barrier.x, barrier.y, TILE_SCALE, TILE_SCALE)) {
       cursorSpr = 46
@@ -258,7 +261,7 @@ function TIC() {
   const nt = time()
   delta = (nt - t) / 1000
   t = nt
-  getInput(mouseData)
+  mouseData = getInput(mouseData)
   update(mouseData, scene, delta)
   draw(scene)
 }
---
+++ src/types.d.ts
@@ -0,0 +1,18 @@
+interface Point {
+  x: number,
+  y: number
+}
+
+interface MouseData {
+  x: number,
+  y: number,
+  p: number,
+  lastClickStamp: number
+}
+
+interface Padding {
+  top: 3,
+  bottom: 1,
+  left: 2,
+  right: 2
+}
